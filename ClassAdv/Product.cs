bc55a29fdc210cdc31026beceaabfd52b6cdafde
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-05-25 14:27:16 +0900

    소멸자 실습

---
+++ ClassAdv/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassAdv
+{
+    internal class Product
+    {
+        private string name;
+        private int price;
+
+        public Product(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        ~Product() 
+        {
+            Console.WriteLine(this.name + "의 소멸자 호출");
+        }
+    }
+}
--- ClassAdv/Program.cs
+++ ClassAdv/Program.cs
@@ -10,6 +10,16 @@ namespace ClassAdv
     {
         static void Main(string[] args)
         {
+            // 소멸자 실습
+            Product product1 = new Product("과자", 1000);
+            Product product2 = new Product("빼빼로", 2000);
+            Product product3 = new Product("닭꼬치", 3000);
+            Product product4 = new Product("탕후루", 4000);
+
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
+            Console.WriteLine("안녕하세요");
         }
     }
 }
